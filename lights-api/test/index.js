ec5f8fa8474ebd67ada63ab7cc13779d288dc6b7
Matt Harrison <hi@matt-harrison.com>
2020-10-29 19:21:58 +0000

    initial node api

--- lights-api/test/index.js
+++ lights-api/test/index.js
@@ -3,20 +3,72 @@
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
 
+const Lights = require('../lib/lights');
+
 const internals = {};
 
 
-const { describe, it } = exports.lab = Lab.script();
+const { describe, it, beforeEach } = exports.lab = Lab.script();
 const expect = Code.expect;
 
 
 describe('lights-api', () => {
 
-    describe('api', () => {
+    let api;
 
-        it('does something', (flags) => {
+    beforeEach(() => {
 
-            expect(1 + 1).to.equal(2);
+        const lights = [
+            { id: 1, name: 'light1', type: 'Extended color light', state: { reachable: true } },
+            { id: 2, name: 'light2', type: 'Extended color light', state: { reachable: false } },
+            { id: 3, name: 'light3', type: 'Another light', state: { reachable: true } },
+            { id: 4, name: 'a-light4', type: 'Extended color light', state: { reachable: true } },
+            { id: 5, name: 'z-light5', type: 'Extended color light', state: { reachable: true } }
+        ];
+
+        api = {
+            lights: {
+                getAll: () => lights,
+                getLightState: (id) => lights.find((light) => light.id === id).state,
+                setLightState: (id, state) => {
+
+                    Object.assign(lights.find((light) => light.id === id).state,  state);
+                }
+            }
+        };
+    });
+
+    describe('getLights()', () => {
+
+        it('returns a list of lights', async (flags) => {
+
+            const lights = await Lights.getLights(api);
+
+            expect(lights).to.equal([{
+                id: 4,
+                name: 'a-light4',
+                type: 'Extended color light',
+                state: { reachable: true }
+            },
+            {
+                id: 1,
+                name: 'light1',
+                type: 'Extended color light',
+                state: { reachable: true }
+            }]);
+        });
+    });
+
+    describe('setLightState()', () => {
+
+        it('sets a light state', async (flags) => {
+
+            const state = await Lights.setLightState(api, 4, { prop: 'erty' });
+
+            expect(state).to.equal({
+                reachable: true,
+                prop: 'erty'
+            });
         });
     });
 });
