dc1c78e5bf50f3872e0602d55501096ffd899d17
Matt Harrison <hi@matt-harrison.com>
2020-10-31 10:35:16 +0000

    use-redux

--- frontend/src/helpers/color-conversion.js
+++ frontend/src/helpers/color-conversion.js
@@ -120,18 +120,18 @@ export function cie_to_rgb(x, y, brightness)
 export function rgb_to_cie(red, green, blue)
 {
 	//Apply a gamma correction to the RGB values, which makes the color more vivid and more the like the color displayed on the screen of your device
-	var red 	= (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
-	var green 	= (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
-	var blue 	= (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
+	red 	= (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
+	green 	= (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
+	blue 	= (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
 
 	//RGB values to XYZ using the Wide RGB D65 conversion formula
-	var X 		= red * 0.664511 + green * 0.154324 + blue * 0.162028;
-	var Y 		= red * 0.283881 + green * 0.668433 + blue * 0.047685;
-	var Z 		= red * 0.000088 + green * 0.072310 + blue * 0.986039;
+	let X 		= red * 0.664511 + green * 0.154324 + blue * 0.162028;
+	let Y 		= red * 0.283881 + green * 0.668433 + blue * 0.047685;
+	let Z 		= red * 0.000088 + green * 0.072310 + blue * 0.986039;
 
 	//Calculate the xy values from the XYZ values
-	var x 		= (X / (X + Y + Z)).toFixed(4);
-	var y 		= (Y / (X + Y + Z)).toFixed(4);
+	let x 		= (X / (X + Y + Z)).toFixed(4);
+	let y 		= (Y / (X + Y + Z)).toFixed(4);
 
 	if (isNaN(x))
 		x = 0;
